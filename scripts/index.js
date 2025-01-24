c1d924697f94d469585d02518bd32a7a7c4c837f
Yazdan <yazdan.hka@gmail.com>
2025-01-24 13:08:17 +0330

    first animation done, second animation done, third animation done, going to opening the divs

--- scripts/index.js
+++ scripts/index.js
@@ -2,8 +2,13 @@ const circles = document.querySelectorAll('.js-2-circle')
 
 circles.forEach(e => {
   e.addEventListener('click', () => {
-    e.style.animation = 'circle-clicked 8s ease 1'; 
+    e.style.animation = 'circle-clicked 6s ease 1'; 
     e.style.animationFillMode = 'forwards';
+
+    const cd = document.querySelector(`.js-cd-${e.dataset.cNumber}`);
+    setTimeout(() => {
+      cd.classList.add('cd-appeared')
+    }, 5500)
       }
     )
   }
