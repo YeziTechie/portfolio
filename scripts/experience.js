e26956c5162efad6e70d43b891cbb7961111a638
Yazdan <yazdan.hka@gmail.com>
2025-02-19 01:59:14 +0330

    20% of experience detail compelete

--- scripts/experience.js
+++ scripts/experience.js
@@ -5,27 +5,25 @@ function sampleClicked (element) {
   if (element.classList.contains('toggle')) {
     year.style.flex = '1';
     year.style.color = 'var(--fg)';
-    year.style.transform = 'scale(1, 1)';
     year.style.marginLeft = 'none';
 
     detail.style.height = '0px';
 
     element.style.boxShadow = '2px -2px 0px var(--t1)';
+    element.style.fontSize = '1rem';
     element.classList.remove('toggle')
   } else {
     year.style.flex = '0';
     year.style.color = 'var(--t2)';
     year.style.marginLeft = 'auto';
-    year.style.transform = 'scale(1.6, 1.6)';
 
     detail.style.height = 'calc-size(max-content, size)';
 
     element.classList.add('toggle')
     element.style.boxShadow = '-10px 10px 30px var(--t1)';
+    element.style.fontSize = '1.2rem';
   }
 }
-
-
 const experiences = document.querySelectorAll('.experience');
 experiences.forEach(element => {
   element.addEventListener('click', () => sampleClicked(element))
