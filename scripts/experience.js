65be51761b5fd65e0c27732879163f3753ac869e
Yazdan <yazdan.hka@gmail.com>
2025-02-22 12:29:08 +0330

    could do it all wiht css, and learnt new stuff on js

--- scripts/experience.js
+++ scripts/experience.js
@@ -1,27 +1,14 @@
-function sampleClicked (element) {
-  const detail = element.querySelector('.detail');
-  const year = element.querySelector('.year');
-
-  if (element.classList.contains('toggle')) {
-    year.style.flex = '1';
-    year.style.color = 'var(--fg)';
-    year.style.marginLeft = 'none';
-
-    detail.style.height = '0px';
-
-    element.classList.remove('toggle')
-  } else {
-    year.style.flex = '0';
-    year.style.color = 'var(--t2)';
-    year.style.marginLeft = 'auto';
-
-    detail.style.height = 'calc-size(max-content, size)';
-
-    element.classList.add('toggle')
-  }
-}
 const experiences = document.querySelectorAll('.experience');
 experiences.forEach(element => {
-  element.addEventListener('click', () => sampleClicked(element))
-  }
-)
+  element.addEventListener('click', () => {
+    element.classList.toggle('toggle');
+  });
+});
+
+const subDetails = document.querySelectorAll('.js-sub-detail');
+subDetails.forEach(e => {
+  e.addEventListener('click', (event) => {
+    event.stopPropagation();
+    e.classList.toggle('toggle');
+  });
+});
